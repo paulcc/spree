a9d554b4815dd1eee55e5d38110f603c9d8959fa
Sonny Cook <sonny@omnamagni.(none)>
2009-05-01 09:59:36 -0600

    add admin UI for editing product groups as well as adding/removing products to/from ProductGroupList type product groups.  The views and partials (as well as actions) end up scattered all over the place and I would prefer that they were collected into one place with the code for the product group subclass they were for.

--- config/routes.rb
+++ config/routes.rb
@@ -64,6 +64,7 @@ ActionController::Routing::Routes.draw do |map|
     admin.resources :products, :has_many => [:variants, :images, :product_properties] do |product|
       product.resources :option_types, :member => {:select => :get, :remove => :get}, :collection => {:available => :get, :selected => :get}
       product.resources :taxons, :member => {:select => :post, :remove => :post}, :collection => {:available => :post, :selected => :get}
+      product.resources :product_groups, :member => {:select => :post, :remove => :post}, :collection => {:available => :post, :selected=> :get}
     end
     admin.resources :images
     admin.resources :option_types
@@ -88,6 +89,9 @@ ActionController::Routing::Routes.draw do |map|
     admin.resources :shipping_categories
     admin.resources :tax_rates
     admin.resource  :tax_settings      
+    admin.resources :product_groups do |product_group|
+      product_group.resources :products, :member => {:select => :post, :remove => :post}, :collection => {:available => :post, :selected=> :get}
+    end
   end                   
 
   
